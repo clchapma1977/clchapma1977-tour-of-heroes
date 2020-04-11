1b70009711beb5a318749a0d4603469c0eae88bd
clchapma1977 <clchapma@ncsu.edu>
2020-04-11 05:02:40 -0400

    1 binaries uploaded

--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -1,14 +1,14 @@
 import { Hero } from './hero';
 
 export const HEROES: Hero[] = [
-  { id: 1, name: 'Dr Nice' },
-  { id: 2, name: 'Narco' },
-  { id: 3, name: 'Bombasto' },
-  { id: 4, name: 'Celeritas' },
-  { id: 5, name: 'Magneta' },
-  { id: 6, name: 'RubberMan' },
-  { id: 7, name: 'Dynama' },
-  { id: 8, name: 'Dr IQ' },
-  { id: 10, name: 'Magma' },
-  { id: 11, name: 'Tornado' }
+  { id: 1, name: 'Dr Nice', imageUrl: 'https://cdn.jsdelivr.net/gh/clchapma1977/clchapma1977-tour-of-heroes@master/src/app/assets/img/stand-up-to-cancer_750x.progressive.jpg'},
+  { id: 2, name: 'Narco', imageUrl: ''},
+  { id: 3, name: 'Bombasto', imageUrl: ''},
+  { id: 4, name: 'Celeritas', imageUrl: ''},
+  { id: 5, name: 'Magneta', imageUrl: ''},
+  { id: 6, name: 'RubberMan', imageUrl: ''},
+  { id: 7, name: 'Dynama', imageUrl: ''},
+  { id: 8, name: 'Dr IQ', imageUrl: ''},
+  { id: 10, name: 'Magma', imageUrl: ''},
+  { id: 11, name: 'Tornado', imageUrl: ''}
 ];
